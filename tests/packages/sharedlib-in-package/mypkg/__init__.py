ce32f09943165cbe55d3adf0af08db35347e322b
Ralf Gommers <ralf.gommers@gmail.com>
2024-10-27 18:40:55 +0100

    TST: add test package with internal shared library, installed in site-packages

---
+++ tests/packages/sharedlib-in-package/mypkg/__init__.py
@@ -0,0 +1,25 @@
+# SPDX-FileCopyrightText: 2024 The meson-python developers
+#
+# SPDX-License-Identifier: MIT
+
+import os
+
+
+def _load_sharedlib():
+    """Load the `libexamplelib.dll` shared library on Windows
+
+    This shared library is installed alongside this __init__.py file. Due to
+    lack of rpath support, Windows cannot find shared libraries installed
+    within wheels. So pre-load it.
+    """
+    if os.name == "nt":
+        from ctypes import WinDLL
+        basedir = os.path.dirname(__file__)
+        dll_path = os.path.join(basedir, "libexamplelib.dll")
+        WinDLL(dll_path)
+
+
+_load_sharedlib()
+
+
+from ._example import example_sum
